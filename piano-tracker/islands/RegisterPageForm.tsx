fc18e1d6960e4e10e08f7d4c2ad3d586e1a31c80
Alextibtab <48660540+Alextibtab@users.noreply.github.com>
2023-02-16 23:19:47 +0000

    Added register form styling

--- piano-tracker/islands/RegisterPageForm.tsx
+++ piano-tracker/islands/RegisterPageForm.tsx
@@ -11,27 +11,48 @@ const RegisterPageForm = () => {
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <label for="email">Email</label>
-      <input
-        type="email"
-        id="email"
-        name="email"
-        onInput={(e) => {
-          if (e.target) setEmail((e.target as HTMLInputElement).value);
-        }}
-      />
-      <label for="password">Password</label>
-      <input
-        type="password"
-        id="password"
-        name="password"
-        onInput={(e) => {
-          if (e.target) setPassword((e.target as HTMLInputElement).value);
-        }}
-      />
-      <label for="password">Confirm Password</label>
-      <input type="password" id="password" name="password" />
+    <form onSubmit={onSubmit} class="flex flex-col py-8 items-center">
+      <div class="flex mx-auto mb-8">
+        <label
+          class="block pr-4 w-[200px] text-white font-bold tracking-wide"
+          for="email"
+        >
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          onInput={(e) => {
+            if (e.target) setEmail((e.target as HTMLInputElement).value);
+          }}
+        />
+      </div>
+      <div class="flex mx-auto mb-8">
+        <label
+          class="block pr-4 w-[200px] text-white font-bold tracking-wide"
+          for="password"
+        >
+          Password
+        </label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          onInput={(e) => {
+            if (e.target) setPassword((e.target as HTMLInputElement).value);
+          }}
+        />
+      </div>
+      <div class="flex mx-auto mb-8">
+        <label
+          class="block pr-4 w-[200px] text-white font-bold tracking-wide"
+          for="password"
+        >
+          Confirm Password
+        </label>
+        <input type="password" id="password" name="password" />
+      </div>
       <input type="submit" value="Register" />
     </form>
   );
--- piano-tracker/routes/register.tsx
+++ piano-tracker/routes/register.tsx
@@ -5,8 +5,12 @@ import RegisterPageForm from "../islands/RegisterPageForm.tsx";
 const RegisterPage = () => {
   return (
     <Layout active="Register">
-      Register Page Content
-      <RegisterPageForm />
+      <div class="flex flex-col text-center items-center bg-gray-800 w-full block mx-10 rounded-md">
+        <h1 class="pt-8 text-3xl text-white font-black tracking-widest uppercase">
+          Register
+        </h1>
+        <RegisterPageForm />
+      </div>
     </Layout>
   );
 };
